b6c76eb15504950c766514c60ca5397eea7fe610
James <afk17tju@uea.ac.uk>
2020-05-16 18:35:03 +0100

    Textures correctly assigned

--- RpgBuildingGenerator/src/building/Building.java
+++ RpgBuildingGenerator/src/building/Building.java
@@ -13,10 +13,10 @@ import java.util.ArrayList;
  */
 public class Building extends BuildingItem {
 
-    
     private ArrayList<Wall> externalWalls;
     private ArrayList<Room> rooms;
     private int wealthIndicator;
+    private floorplanner.FloorPlanner.BuildingTheme buildingTheme;
     
     public Building() {
         super();
@@ -49,6 +49,14 @@ public class Building extends BuildingItem {
         return wealthIndicator;  
     }
     
+    public void setBuildingTheme(floorplanner.FloorPlanner.BuildingTheme theme) {
+        this.buildingTheme = theme;
+    }
+    
+    public floorplanner.FloorPlanner.BuildingTheme getBuildingTheme() {
+        return this.buildingTheme;
+    }
+    
     public void Generate3DPositions() {
     
         // External walls - One texture for all external walls
@@ -65,19 +73,62 @@ public class Building extends BuildingItem {
     
         
     private String chooseExternalWallTexture() {
-        if (this.wealthIndicator <= 50) {
-            if (Math.random() < 0.5) {
-                return "stone_wall2.png";                        
-            } else {
-                return "stone_wall.png";     
-            }
-        } else {
-            if (Math.random() < 0.5) {
-                return "brick_wall.png";                        
-            } else {
-                return "brick_wall2.png";     
-            }
+        
+        switch (this.buildingTheme) {
+            case MEDIEVAL:
+                System.out.println(this.buildingTheme + "  :  "+this.wealthIndicator);
+                if (this.wealthIndicator <= 15) {                                   // STRAW
+                    return "Straw_wall1.jpg"; 
+                }
+                else if (this.wealthIndicator > 15 && this.wealthIndicator <= 35) { // WOOD (POOR)
+                    return "Wood_wall1.png"; 
+                }
+                else if (this.wealthIndicator > 35 && this.wealthIndicator <= 50) { // WOOD (RICH)
+                    if (Math.random() < 0.5) {                                      // STONE (POOR)
+                        return "Wood_wall2.png";
+                    }
+                    else {                                    
+                        return "Stone_wall2.png";
+                    }
+                }
+                else if (this.wealthIndicator > 50 && this.wealthIndicator <= 70) { // WOOD (RICH)
+                    double n = Math.random();                                       // STONE (RICH)
+                    if (n <= 0.4) {                                                 
+                        return "Wood_wall2.png";
+                    }
+                    else {
+                        return "Stone_wall.png";
+                    }
+                }
+                else if (this.wealthIndicator > 70 && this.wealthIndicator <= 85) { // LIMESTONE
+                    double n = Math.random();                                       // STONE (RICH)
+                    if (n <= 0.4) {                                                 // MARBLE
+                        return "limestone_wall.png";
+                    }
+                    else if (n > 0.4 && n <= 0.7){
+                        return "Stone_wall.png";
+                    }
+                    else {
+                        return "Marble_wall.png";
+                    }
+                }
+                else {                                                              // MARBLE
+                    if (Math.random() < 0.6) {                                      // LIMESTONE
+                        return "limestone_wall.png";
+                    }
+                    else {                                    
+                        return "Marble_wall.png";
+                    }
+                }   
+            case MODERN:
+                if (Math.random() < 0.5) {
+                    return "brick_wall.png";                        
+                } else {
+                    return "brick_wall2.png";     
+                }
+            default: // FUTURISTIC
+                return "Metal_wall.png"; 
         }
+                
     }
-    
 }
