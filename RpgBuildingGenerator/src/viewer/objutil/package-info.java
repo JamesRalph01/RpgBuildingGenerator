9f92d64fe185040b73fedb9e86c8aedd828c2567
chrisralph <chrisralph@chriss-air>
2020-05-20 15:01:55 +0100

    Added new Triangulation routine - only works if points are defined clockwise

---
+++ RpgBuildingGenerator/src/viewer/objutil/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Classes for reading and writing Wavefront OBJ and MTL files.
+ * <p>
+ * {@link viewer.objutil.Obj} objects may be created with
+ * {@link viewer.objutil.Objs#create()} or by reading an OBJ file with
+ * {@link viewer.objutil.ObjReader#read(java.io.InputStream)}.
+ * {@link viewer.objutil.Obj} objects may be written as OBJ files with
+ * {@link viewer.objutil.ObjWriter#write(ReadableObj, java.io.OutputStream)}.
+ * <br>
+ */
+package viewer.objutil;
+
